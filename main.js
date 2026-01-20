378f9bace0bfcb79358568a1bc92e5a048b58d75
42 <3384898611@qq.com>
2026-01-21 00:19:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
+  if (mySrc === https://th.bing.com/th/id/R.8bbf769b39bb26eefb9b6de51c23851d?rik=crTnc5i8A%2b8p7A&riu=http%3a%2f%2fpicview.iituku.com%2fcontentm%2fzhuanji%2fimg%2f202207%2f09%2fe7196ac159f7cf2b.jpg%2fnu&ehk=DYPLVpoNAXLj5qzwgR5vHf9DladFh%2b34s4UcuP3Kn6E%3d&risl=&pid=ImgRaw&r=0");
+ ) {
     myImage.setAttribute("src", "https://th.bing.com/th/id/R.8bbf769b39bb26eefb9b6de51c23851d?rik=crTnc5i8A%2b8p7A&riu=http%3a%2f%2fpicview.iituku.com%2fcontentm%2fzhuanji%2fimg%2f202207%2f09%2fe7196ac159f7cf2b.jpg%2fnu&ehk=DYPLVpoNAXLj5qzwgR5vHf9DladFh%2b34s4UcuP3Kn6E%3d&risl=&pid=ImgRaw&r=0");
   } else {
     myImage.setAttribute("src", "https://cdn.pixabay.com/photo/2020/04/13/19/40/sun-5039871_1280.jpg");
@@ -23,4 +24,5 @@ if (!localStorage.getItem("name")) {
 }
 myButton.addEventListener("click", () => {
   setUserName();
+
 });
